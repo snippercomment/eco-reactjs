fcaec0dea2443b59e3700ec7d932a70b34bf70b6
snippercomment <minhthuanad2002@gmail.com>
2025-12-22 20:57:02 +0700

    fix bug by one day

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
+import BoxIcon from './BoxIcon/BoxIcon';
+import { dataBoxIcon } from './constants';
+import styles from './style.module.scss';
+
 function MyHeader() {
+    const { containerBoxIcon } = styles;
     return (
         <div>
-            <div></div>
+            <div>
+                <div className='containerBoxIcon'>
+                    {dataBoxIcon.map(item => {
+                        return <BoxIcon type={item.type} href={item.href} />;
+                    })}
+                </div>
+                <div></div>
+            </div>
             <div></div>
             <div></div>
         </div>
---
+++ src/components/Header/constants.js
@@ -0,0 +1,16 @@
+const dataBoxIcon = [
+    { type: 'fb', href: '#' },
+    { type: 'ins', href: '#' },
+    { type: 'ytb', href: '#' },
+];
+
+const dataMenu = [
+    { content: 'Elements', href: '#' },
+    { content: 'Out SHop', href: '#' },
+    { content: 'About Us', href: '#' },
+    { content: 'Contacts', href: '#' },
+    { content: 'Search', href: '#' },
+    { content: 'Sign In', href: '#' },
+];
+
+export { dataBoxIcon, dataMenu };
